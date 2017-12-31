11b864d919698c0cbfe7a7679873e6be563b4d3e
MariacristinaOrtiz <mari_d17@hotmail.com>
2017-12-31 10:06:41 -0500

    Añade evento click para añadir el modal para cada restaurant, evento click para volver a la pagina principal al cerrar el modal

--- js/data.js
+++ js/data.js
@@ -1,8 +1,15 @@
 
-// Data de los restaurantes por paises(tipo de comida por paises)
+// Data de los restaurantes(tipo de comida por paises)
 var restaurants = {
   'comida argentina': [
-    {url: 'a.costumbres.argentina1.png'},
+    {url: 'a.costumbres.argentina1.png',
+      name: 'Costumbres Argentinas',
+      map: '<iframe class="img-responsive" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.7139664373262!2d-77.02021668573141!3d-12.131712746788308!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b7fdba135dcf%3A0x2a09ac470411ad13!2sCostumbres+Argentinas!5e0!3m2!1ses!2spe!4v1514730840174" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>',
+      website: 'http://costumbresargentinas.com.pe/',
+      indications: 'https://www.google.com.pe/maps/dir/""/costumbres+argentinas/data=!4m5!4m4!1m0!1m2!1m1!1s0x9105b7fdba135dcf:0x2a09ac470411ad13?sa=X&ved=0ahUKEwjyr73607PYAhUFct8KHUmDADEQ9RcIyQEwCw',
+      address: 'Av. República de Panamá 6562, Barranco',
+      phone: '(01) 4441581',
+      reservations: 'http://costumbresargentinas.com.pe/', },
     {url: 'a.el10.argentina2.png'},
     {url: 'a.tranquera.argentina3.png'},
     {url: 'a.vacaloca.argentina4.png'},
--- js/principalview.js
+++ js/principalview.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
-  // declaración de variables
+  // DECLARACION DE VARIABLES
   var rutaLocal = '../assets/imgs/';
-  // selección de elementos y guardados en variables
+  // SELECCION DE ELEMENTOS GUARDADOS EN VARIBALES
   var $inputSearch = $('#search-for');
   var $divContainer = $('.content-search');
   var $generalImage = $('#general-img');
-  // funciones para los eventos
+  // seleccion de elementos del modal
+  var $restaurant = $('#restaurant');
+  var $map = $('#map');
+  var $website = $('#website');
+  var $indications = $('#indications');
+  var $address = $('#address');
+  var $phone = $('#phone');
+  var $reservations = $('#reservation');
+  var $close = $('#close');
+  // ...........FUNCIONES PARA LOS EVENTOS KEYUP............
+  // FUNCIÓN PRIMERA: para DESAPARECER la imagen general
   function inputEventOne(event) {
     if ($(this).val() === '') {
       $generalImage.fadeIn('slow');
@@ -13,20 +23,52 @@ $(document).ready(function() {
       $generalImage.fadeOut('slow');
     }
   }
+  // FUNCIÓN SEGUNDA: para FILTRAR LAS IMAGENES(restaurants) y mostral el modal por restaurant
   function inputEventTwo(event) {
     $.each(restaurants, function(key, val) {
       if ($inputSearch.val() === key) {
         for (var i in val) {
-          $divContainer.append('<figure class = "col-xs-6 food"><img class = "image-responsive" src = ' + rutaLocal + val[i].url + ' ></figure>');
+          $divContainer.append('<figure class = "col-xs-6 col-sm-4 food"><img class = "image-responsive"  src = ' + rutaLocal + val[i].url + ' alt = ' + val[i].url + ' data-toggle="modal" data-target="#myModal"></figure>');
         }
       } else if ($inputSearch.val() === '' || $inputSearch.val() === 'comida ') {
-        var $figureFood = $('.food');
-        $figureFood.remove();
+        $('.food').remove();
       }
     });
+    // EVENTO MOUSEOVER: Dentro de la FUNCION SEGUNDA donde se creó la etiqueta "figure".
+    var $figureFood = $('.food');
+    $figureFood.each(function(i, val) {
+      $figureFood.children().on('mouseover', function() {
+        $(this).eq(i).children().fadeTo(800, 0.25);
+        $(this).eq(i).children().fadeTo(800, 1);
+      });
+    });
+    // EVENTO CLICK PARA AGREGAR MODAL: Dentro de la FUNCION SEGUNDA donde se creó la etiqueta "figure".
+    $figureFood.each(function(i, el) {
+      $figureFood.eq(i).on('click', function() {
+        $.each(restaurants, function(key, val) {
+          if ($figureFood.eq(i).children().attr('alt') === val[i].url) {
+            $restaurant.append(val[i].name);
+            $map.append(val[i].map);;
+            $address.append(val[i].address);
+            $phone.append(val[i].phone);
+            $indications.attr('href', val[i].indications);
+            $website.attr('href', val[i].website);
+            $reservations.attr('href', val[i].reservations);
+          }
+        });
+      });
+    });
   }
-  // eventos
+  // ......FUNCION PARA EVENTO CLICK: Al cerrar el modal(click) se volvera a la vidta principal............
+  function closeModalEvent() {
+    window.location.href = 'principalview.html';
+  };
+  // EVENTOS
+  // Evento que llama a las FUNCIONES 'PRIMERA Y SEGUNDA'
   $inputSearch
     .on('keyup', inputEventOne)
     .on('keyup', inputEventTwo);
+  // Evento que llama a la 'FUNCION PARA EL EVENTO CLICK'
+  $close
+    .on('click', closeModalEvent);
 });
