0f1e2c845877e6c368215da6f0ca95b5aa1078eb
MariacristinaOrtiz <mari_d17@hotmail.com>
2017-12-31 12:10:52 -0500

    añade data.js completa

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-/*$(document).ready(function() {
+$(document).ready(function() {
+  // Despues de 5 segundos aparecera la vista principal.
   setTimeout(function() {
     window.location.href = 'views/principalview.html';
   }, 5000);
-});*/
+});
